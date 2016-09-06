17677a05b1718f52c8f4086365b2c8584df1d498
Vadim Yakhin <yakhin.v@gmail.com>
2016-09-06 21:22:56 +0300

    refactor: InputWords doublequotes -> singlequotes

--- src/components/InputWords/InputWords.js
+++ src/components/InputWords/InputWords.js
@@ -4,8 +4,8 @@ export default class InputWords extends React.Component {
   render() {
     return (
       <input
-        name="words"
-        type="text"
+        name='words'
+        type='text'
         value={this.props.value}
         onChange={this.props.handleChange}
       />
