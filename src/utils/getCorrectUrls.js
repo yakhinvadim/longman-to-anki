23752d5ed9e2ae180cd21f66936715cacaaa3097
Vadim Yakhin <yakhin.v@gmail.com>
2016-09-12 16:58:49 +0300

    feature: fix urls in getCorrectUrls

--- src/utils/getCorrectUrls.js
+++ src/utils/getCorrectUrls.js
@@ -1,7 +1,7 @@
 import cheerio from 'cheerio';
 
 export default function getCorrectUrls(word) {
-  return fetch(`http://www.ldoceonline.com/search/?q=${word}`)
+  return fetch(`https://query.yahooapis.com/v1/public/yql?q=SELECT%20*%20FROM%20html%20WHERE%20url%3D%22http%3A%2F%2Fwww.ldoceonline.com%2Fsearch%2F%3Fq%3D${word}%22&diagnostics=true`)
     .then(resp => {
       return resp.text()
     })
@@ -18,11 +18,11 @@ export default function getCorrectUrls(word) {
     })
     .then(urlsNumber => {
       if(urlsNumber < 2) {
-        return [`http://www.ldoceonline.com/dictionary/${word}`]
+        return [`https://query.yahooapis.com/v1/public/yql?q=SELECT%20*%20FROM%20html%20WHERE%20url%3D%22http%3A%2F%2Fwww.ldoceonline.com%2Fdictionary%2F${word}%22&diagnostics=true`]
       } else {
-        correctUrls = [];
+        let correctUrls = [];
         for(let i = 1; i <= urlsNumber; i++) {
-          correctUrls.push(`http://www.ldoceonline.com/dictionary/${word}_${i}`)
+          correctUrls.push(`https://query.yahooapis.com/v1/public/yql?q=SELECT%20*%20FROM%20html%20WHERE%20url%3D%22http%3A%2F%2Fwww.ldoceonline.com%2Fdictionary%2F${word}_${i}%22&diagnostics=true`)
         }
         return correctUrls;
       }
