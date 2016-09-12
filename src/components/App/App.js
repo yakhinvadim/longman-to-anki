c5718535e99c18da26204f413f1a06be3232f7ae
Vadim Yakhin <yakhin.v@gmail.com>
2016-09-12 16:12:32 +0300

    add url fetching using Yahoo YQL

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,7 +4,8 @@ import './App.css';
 
 export default class App extends React.Component {
   state = {
-    inputValue: ''
+    inputValue: '',
+    wordData: ''
   }
 
   handleInputChange = (event) => {
@@ -13,14 +14,35 @@ export default class App extends React.Component {
     })
   }
 
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.fetchWordData(this.state.inputValue)
+  }
+
+  fetchWordData = (word) => {
+    fetch(`https://query.yahooapis.com/v1/public/yql?q=SELECT%20*%20FROM%20html%20WHERE%20url%3D%22http%3A%2F%2Fwww.ldoceonline.com%2Fdictionary%2F${word}%22&diagnostics=true`)
+      .then(resp => resp.text())
+      .then(data => this.setState({
+        wordData: data
+      }))
+  }
+
   render() {
     return (
-      <form>
-        <InputWords
-          value={this.state.inputValue}
-          handleChange={this.handleInputChange}
-        />
-      </form>
+      <div>
+        <form
+          onSubmit={this.handleSubmit}
+        >
+          <InputWords
+            value={this.state.inputValue}
+            handleChange={this.handleInputChange}
+          />
+        </form>
+
+        <div className="word-data">
+          {this.state.wordData}
+        </div>
+      </div>
     );
   }
 }
