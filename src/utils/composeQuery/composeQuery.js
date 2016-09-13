d5bb2ac4a823c78ac64940c1f3df18fba1b182c9
Vadim Yakhin <yakhin.v@gmail.com>
2016-09-13 12:11:13 +0300

    feature: make search query default in composeQuery

--- src/utils/composeQuery/composeQuery.js
+++ src/utils/composeQuery/composeQuery.js
@@ -1,8 +1,8 @@
 export default function composeQuery({ type, word }) {
-  const queryStr = type === 'search'
-    ? 'search/?q=' : type === 'entry'
-    ? 'dictionary/'
-    : 'dictionary/';
+  const queryStr = type === 'entry'
+    ? 'dictionary/' : type === 'search'
+    ? 'search/?q='
+    : 'search/?q=';
 
   return `https://query.yahooapis.com/v1/public/yql?q=${encodeURIComponent(
     `SELECT * FROM html WHERE url="http://www.ldoceonline.com/${queryStr}${word}"`
--- src/utils/composeQuery/composeQuery.test.js
+++ src/utils/composeQuery/composeQuery.test.js
@@ -20,6 +20,6 @@ it('returns correct query when type is not set', () => {
   expect(
     composeQuery({ word: 'deliberately' })
   ).toEqual(
-    'https://query.yahooapis.com/v1/public/yql?q=SELECT%20*%20FROM%20html%20WHERE%20url%3D%22http%3A%2F%2Fwww.ldoceonline.com%2Fdictionary%2Fdeliberately%22'
+    'https://query.yahooapis.com/v1/public/yql?q=SELECT%20*%20FROM%20html%20WHERE%20url%3D%22http%3A%2F%2Fwww.ldoceonline.com%2Fsearch%2F%3Fq%3Ddeliberately%22'
   )
 });
