aee7b946f72cea5e72d879813afca147c7326bf5
Vadim Yakhin <yakhin.v@gmail.com>
2016-09-14 22:51:22 +0300

    refactor: refactor getCorrecturls a little using Ramda

--- src/utils/getCorrectUrls/getCorrectUrls.js
+++ src/utils/getCorrectUrls/getCorrectUrls.js
@@ -1,31 +1,29 @@
 import cheerio from 'cheerio';
-import composeQuery from '../composeQuery/composeQuery.js'
+import composeQuery from '../composeQuery/composeQuery.js';
+import R from 'ramda';
 
 export default function getCorrectUrls(word) {
   return fetch(composeQuery({ type: 'search', word: word }))
-    .then(resp => {
-      return resp.text()
-    })
+    .then(resp => resp.text())
     .then(body => {
       let $ = cheerio.load(body);
       const urlsNumber =
         $('span.headword')
           .map((i, item) => $(item).text())
           .get()
-          .filter(currentWord => currentWord === word)
+          .filter(R.equals(word))
           .length;
 
       return urlsNumber;
     })
     .then(urlsNumber => {
       if(urlsNumber < 2) {
-        return [composeQuery({ type: 'entry', word: word })]
+        return R.identity([composeQuery({ type: 'entry', word: word })])
       } else {
-        let correctUrls = [];
-        for(let i = 1; i <= urlsNumber; i++) {
-          correctUrls.push(composeQuery({ type: 'entry', word: `${word}_${i}` }))
-        }
-        return correctUrls;
+        return R.times(
+          i => composeQuery({ type: 'entry', word: `${word}_${i + 1}` }),
+          urlsNumber
+        )
       }
     })
     .catch(err => console.log(err));
