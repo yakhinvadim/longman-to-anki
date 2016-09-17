e69d2bec74db96a1de67700710cc08550cab7c1d
Vadim Yakhin <yakhin.v@gmail.com>
2016-09-17 21:43:11 +0300

    beautify: composeDictionaryEntry

--- src/utils/composeDictionaryEntry/composeDictionaryEntry.js
+++ src/utils/composeDictionaryEntry/composeDictionaryEntry.js
@@ -4,13 +4,34 @@ export default function composeDictionaryEntry(body) {
   let $ = cheerio.load(body);
 
   function getExample(exampleNumber) {
-    return $('.EXAMPLE').eq(exampleNumber).text().trim();
+    return $('.EXAMPLE')
+      .eq(exampleNumber)
+      .text()
+      .trim();
   }
 
   function getForm(exampleNumber) {
-    var lexUnit = $('.EXAMPLE').eq(exampleNumber).parent().prevAll('h2').children('.LEXUNIT').text().trim() +
-      $('.EXAMPLE').eq(exampleNumber).parent().prevAll('strong').text().trim();
-    var phrasalVerb = $('.EXAMPLE').eq(exampleNumber).parents('.PhrVbEntry').find('.phrvbhwdsel').text().trim();
+    var lexUnit = $('.EXAMPLE')
+      .eq(exampleNumber)
+      .parent()
+      .prevAll('h2')
+      .children('.LEXUNIT')
+      .text()
+      .trim()
+      + $('.EXAMPLE')
+      .eq(exampleNumber)
+      .parent()
+      .prevAll('strong')
+      .text()
+      .trim();
+
+    var phrasalVerb = $('.EXAMPLE')
+      .eq(exampleNumber)
+      .parents('.PhrVbEntry')
+      .find('.phrvbhwdsel')
+      .text()
+      .trim();
+
     if (lexUnit.length !== 0 && phrasalVerb.length !== 0) {
       return lexUnit + '<br>' + phrasalVerb;
     } else
@@ -18,28 +39,58 @@ export default function composeDictionaryEntry(body) {
   }
 
   function getTerm() {
-    return $('.HWD').text().trim();
+    return $('.HWD')
+      .text()
+      .trim();
   }
 
   function getGeography(exampleNumber) {
-    return $('.EXAMPLE').eq(exampleNumber).parent().prevAll('.GEO').text().trim();
+    return $('.EXAMPLE')
+      .eq(exampleNumber)
+      .parent()
+      .prevAll('.GEO')
+      .text()
+      .trim();
   }
 
   function getUsage(exampleNumber) {
-    return $('.EXAMPLE').eq(exampleNumber).parent().prevAll('.REGISTERLAB').text().trim();
+    return $('.EXAMPLE')
+      .eq(exampleNumber)
+      .parent()
+      .prevAll('.REGISTERLAB')
+      .text()
+      .trim();
   }
 
   function getDefinition(exampleNumber) {
-    return $('.EXAMPLE').eq(exampleNumber).parent().prevAll('ftdef').children('.DEF').text().trim() +
-      $('.EXAMPLE').eq(exampleNumber).parent().parent().prevAll('ftdef').children('.DEF').text().trim();
+    return $('.EXAMPLE')
+      .eq(exampleNumber)
+      .parent()
+      .prevAll('ftdef')
+      .children('.DEF')
+      .text()
+      .trim()
+      + $('.EXAMPLE')
+      .eq(exampleNumber)
+      .parent()
+      .parent()
+      .prevAll('ftdef')
+      .children('.DEF')
+      .text()
+      .trim();
   }
 
   function composeCard(exampleNumber) {
     var example = '<span class="example">' + getExample(exampleNumber) + '</span><br><br>';
+
     var form = getForm(exampleNumber).length ? '<span class="form">' + getForm(exampleNumber) + '</span><br><br>' : '';
+
     var term = '<span class="term">' + getTerm() + '</span>';
+
     var geography = getGeography(exampleNumber).length ? '<span class="term">' + getGeography(exampleNumber) + '</span> ' : '';
+
     var usage = getUsage(exampleNumber).length ? '<span class="usage">' + getUsage(exampleNumber) + '</span><br>' : '';
+
     var definition = '<span class="definition">' + getDefinition(exampleNumber) + '</span>';
 
     /*
