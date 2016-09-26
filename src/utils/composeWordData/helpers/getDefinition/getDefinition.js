eb92c6343179b12fe47b60cd3e95bddf20817473
Vadim Yakhin <yakhin.v@gmail.com>
2016-09-26 21:28:46 +0300

    bugfix: set correct getDefinition function name

--- src/utils/composeWordData/helpers/getDefinition/getDefinition.js
+++ src/utils/composeWordData/helpers/getDefinition/getDefinition.js
@@ -1,6 +1,6 @@
 import cheerio from 'cheerio';
 
-export default function splitByEntries(senseMarkup) {
+export default function getDefinition(senseMarkup) {
   const $ = cheerio.load(senseMarkup, { decodeEntities: false });
 
   return $('.DEF').text().trim();
