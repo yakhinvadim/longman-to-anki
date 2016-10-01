bf2478f5250ca81417afdaef3e7135cb71d563b5
Vadim Yakhin <yakhin.v@gmail.com>
2016-10-01 16:29:40 +0300

    feature: update composeDictionaryEntry tests for new site structure

--- src/utils/getCorrectUrls/getCorrectUrls.test.js
+++ src/utils/getCorrectUrls/getCorrectUrls.test.js
@@ -11,26 +11,20 @@ it('returns correct urls for 1 page word "deliberately"', () => {
 it('returns correct urls for 2 pages word "crank"', () => {
   return getCorrectUrls("crank")
     .then(urls => expect(urls).toEqual([
-      composeQuery({ type: 'entry', word: 'crank_1' }),
-      composeQuery({ type: 'entry', word: 'crank_2' })
+      composeQuery({ type: 'entry', word: 'crank' })
     ]))
 });
 
 it('returns correct urls for 3 pages word "front"', () => {
   return getCorrectUrls("front")
     .then(urls => expect(urls).toEqual([
-      composeQuery({ type: 'entry', word: 'front_1' }),
-      composeQuery({ type: 'entry', word: 'front_2' }),
-      composeQuery({ type: 'entry', word: 'front_3' })
+      composeQuery({ type: 'entry', word: 'front' })
     ]))
 });
 
 it('returns correct urls for 4 pages word "cool"', () => {
   return getCorrectUrls("cool")
     .then(urls => expect(urls).toEqual([
-      composeQuery({ type: 'entry', word: 'cool_1' }),
-      composeQuery({ type: 'entry', word: 'cool_2' }),
-      composeQuery({ type: 'entry', word: 'cool_3' }),
-      composeQuery({ type: 'entry', word: 'cool_4' }),
+      composeQuery({ type: 'entry', word: 'cool' })
     ]))
 });
