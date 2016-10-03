2ac71eb0775ae16cdf3f3689ec25264368b15aa5
Vadim Yakhin <yakhin.v@gmail.com>
2016-10-03 20:37:55 +0300

    feature: write better tests for getDefinition

--- src/utils/composeWordData/helpers/getDefinition/getDefinition.test.js
+++ src/utils/composeWordData/helpers/getDefinition/getDefinition.test.js
@@ -1,33 +1,22 @@
 import getDefinition from './getDefinition.js';
-import deliberately from './../../mocks/deliberately';
 import set from './../../mocks/set';
-import R from 'ramda';
 
 describe('getDefinition', () => {
-  it('gets correct definitions for "deliberately" entry', () => {
+  it('gets correct definition for normal sense', () => {
     expect(
-      R.map(getDefinition, deliberately.sensesMarkup)
-    ).toEqual([
-      'done in a way that is intended or planned',
-      'done or said in a slow careful way'
-    ])
+      getDefinition(set.sensesMarkup2[0])
+    ).toEqual('a group of similar things that belong together or are related in some way')
   });
 
-  it('gets correct definitions for "set" second entry', () => {
+  it('gets correct definitions for crossref sense', () => {
     expect(
-      R.map(getDefinition, set.sensesMarkup2)
-    ).toEqual([
-      'a group of similar things that belong together or are related in some way',
-      'a television, or a piece of equipment for receiving radio signals',
-      'a place where a film or television programme is filmed',
-      'the scenery, furniture etc used on a stage in a play or in the place where a film or television show is being made',
-      'one part of a game such as tennis or volleyball',
-      'a group of people who are similar in some way and spend time together socially',
-      '',
-      'a performance by a singer, band, or disc jockey',
-      'a group of numbers, shapes etc in mathematics',
-      'a group of children who are taught a particular school subject together because they have the same level of ability in that subject',
-      'a small onion that you plant in order to grow bigger ones'
-    ])
+      getDefinition(set.sensesMarkup2[6])
+    ).toEqual('')
+  });
+
+  xit('gets correct definitions for sense with subsenses', () => {
+    expect(
+      getDefinition(set.sensesMarkup1[1])
+    ).toEqual('if a doctor sets a broken bone, he or she moves it into position so that the bone can grow together again\nOR\nif a broken bone sets, it joins together again')
   });
 });
