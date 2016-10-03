32ed235df428392bcd04ea68ffdfcc502087f43f
Vadim Yakhin <yakhin.v@gmail.com>
2016-10-03 20:44:49 +0300

    feature: write better tests for splitByClass

--- src/utils/composeWordData/helpers/splitByClass/splitByClass.test.js
+++ src/utils/composeWordData/helpers/splitByClass/splitByClass.test.js
@@ -1,14 +1,7 @@
 import splitByClass from './splitByClass.js';
-import deliberately from './../../mocks/deliberately';
 import set from './../../mocks/set';
 
-describe('splitByClass form .Entry', () => {
-  it('splits "deliberately" pageMarkup correctly', () => {
-    expect(
-      splitByClass('.Entry', deliberately.pageMarkup)
-    ).toEqual(deliberately.entriesMarkup)
-  });
-
+describe('splitByClass for .Entry', () => {
   it('splits "set" pageMarkup correctly', () => {
     expect(
       splitByClass('.Entry', set.pageMarkup)
@@ -17,18 +10,6 @@ describe('splitByClass form .Entry', () => {
 });
 
 describe('splitByClass for .Sense', () => {
-  it('splits "deliberately" entryMarkup correctly', () => {
-    expect(
-      splitByClass('.Sense', deliberately.entriesMarkup[0])
-    ).toEqual(deliberately.sensesMarkup)
-  });
-
-  it('splits "set" first entryMarkup by correct number of senses', () => {
-    expect(
-      splitByClass('.Sense', set.entriesMarkup[0]).length
-    ).toEqual(25)
-  });
-
   it('splits "set" second entryMarkup correctly', () => {
     expect(
       splitByClass('.Sense', set.entriesMarkup[1])
