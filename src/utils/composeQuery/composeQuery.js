db9fa03d10efc2841a109e723473d96ba3cc0aa7
Vadim Yakhin <yakhin.v@gmail.com>
2016-10-03 21:36:11 +0300

    feature: remove 'search' queryType from composeQuery, write better tests

--- src/utils/composeQuery/composeQuery.js
+++ src/utils/composeQuery/composeQuery.js
@@ -1,9 +1,5 @@
-export default function composeQuery({ type, word }) {
-  const queryStr = type === 'entry'
-    ? 'dictionary/'
-    : 'search/?q=';
-
-  const dictionaryUrl = `http://www.ldoceonline.com/${queryStr}${word}`;
+export default function composeQuery(word) {
+  const dictionaryUrl = `http://www.ldoceonline.com/dictionary/${word}`;
 
   const queryUrl = `https://query.yahooapis.com/v1/public/yql?q=${encodeURIComponent(
     `SELECT * FROM html WHERE url="${dictionaryUrl}"`
