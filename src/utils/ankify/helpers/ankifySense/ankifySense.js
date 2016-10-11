9655abe7a9bbd48721285a79f10cb3887c6b86a1
Vadim Yakhin <yakhin.v@gmail.com>
2016-10-11 22:49:03 +0300

    feature: add ankifySense function and tests

---
+++ src/utils/ankify/helpers/ankifySense/ankifySense.js
@@ -0,0 +1,16 @@
+import R from 'ramda';
+import ankifyExample from '../ankifyExample/ankifyExample.js';
+
+const ankifySenseNotCurried = (headword, sense) => {
+  const cards = R.pipe(
+    R.prop('examples'),
+    R.map(ankifyExample(headword, sense.definition)),
+    R.join('\n')
+  )(sense);
+
+  return cards;
+}
+
+const ankifySense = R.curry(ankifySenseNotCurried);
+
+export default ankifySense;
---
+++ src/utils/ankify/helpers/ankifySense/ankifySense.test.js
@@ -0,0 +1,28 @@
+import ankifySense from './ankifySense.js';
+
+const headword = 'headword';
+
+const sense = {
+  definition: 'definition',
+  examples: [
+    {
+      text: 'example1'
+    },
+    {
+      text: 'example2',
+    }
+  ]
+};
+
+const ankiCards =
+  'example1<br><br>headword#definition' +
+  '\n' +
+  'example2<br><br>headword#definition';
+
+describe('ankifySense', () => {
+  it('composes correct ankiCards ', () => {
+    expect(
+      ankifySense(headword, sense)
+    ).toBe(ankiCards);
+  });
+});
