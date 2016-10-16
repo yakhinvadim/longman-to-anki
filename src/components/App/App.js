d6d3cfa94a415fe388307e47a09a9c37ff45bde1
Vadim Yakhin <yakhin.v@gmail.com>
2016-10-16 20:29:45 +0300

    feature: add showing import options on download

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,12 +2,22 @@ import React from 'react';
 import ankifyWords from '../../utils/ankifyWords/ankifyWords';
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
+
 import './App.css';
+import importOptions from './images/import-options.png';
+
+const importOptionsAlt = `
+  Import options:
+  Fiels separated by: '#',
+  Ignore lines where first field matches existing note,
+  Allow HTML in fields
+`;
 
 export default class App extends React.Component {
   state = {
     inputValue: '',
-    wordData: ''
+    wordData: '',
+    showImportOptions: false
   }
 
   handleInputChange = (event) => {
@@ -26,6 +36,12 @@ export default class App extends React.Component {
       .catch(err => console.log(err));
   }
 
+  handleDownload = () => {
+    this.setState({
+      showImportOptions: true
+    })
+  }
+
   render() {
     return (
       <div className='App'>
@@ -37,6 +53,7 @@ export default class App extends React.Component {
             floatingLabelText="Type some words, you want to learn"
             value={this.state.inputValue}
             onChange={this.handleInputChange}
+            fullWidth
           />
           <RaisedButton
             label="Compose cards"
@@ -46,11 +63,13 @@ export default class App extends React.Component {
         </form>
 
         <TextField
+          hintText="Text for anki cards will appear here"
           multiLine
           fullWidth
           value={this.state.wordData}
           rowsMax={10}
         />
+
         <RaisedButton
           label="Save as txt"
           primary
@@ -59,6 +78,23 @@ export default class App extends React.Component {
           buttonStyle={{
             verticalAlign: 'initial'
           }}
+          onClick={this.handleDownload}
+          disabled={!this.state.wordData}
+        />
+
+        <span
+          style={{
+            marginLeft: 20,
+            display: this.state.showImportOptions ? 'inline' : 'none'
+          }}
+        >Set the import options as on the sample</span>
+        <img
+          style={{
+            marginTop: 20,
+            display: this.state.showImportOptions ? 'block' : 'none'
+          }}
+          alt={importOptionsAlt}
+          src={importOptions}
         />
       </div>
     );
