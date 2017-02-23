a476d23a05f213fe90edafdd613e8720461d73d1
Vadim Yakhin <yakhin.v@gmail.com>
2017-02-23 16:59:41 +0300

    feature: add possibility to create cards for collocations

--- src/utils/composeQuery/composeQuery.js
+++ src/utils/composeQuery/composeQuery.js
@@ -1,5 +1,11 @@
+import R from 'ramda';
+
+const spacesToHyphens = R.replace(/ /g, '-');
+
 export default function composeQuery(word) {
-  const dictionaryUrl = `http://www.ldoceonline.com/dictionary/${word}`;
+  const escapedWord = spacesToHyphens(word);
+
+  const dictionaryUrl = `http://www.ldoceonline.com/dictionary/${escapedWord}`;
 
   const queryUrl = `https://query.yahooapis.com/v1/public/yql?q=${encodeURIComponent(
     `SELECT * FROM html WHERE url="${dictionaryUrl}"`
--- src/utils/composeQuery/composeQuery.test.js
+++ src/utils/composeQuery/composeQuery.test.js
@@ -8,4 +8,12 @@ describe('composeQuery', () => {
       'https://query.yahooapis.com/v1/public/yql?q=SELECT%20*%20FROM%20html%20WHERE%20url%3D%22http%3A%2F%2Fwww.ldoceonline.com%2Fdictionary%2Fset%22'
     )
   });
+
+  it('returns correct query for word with spaces inside', () => {
+    expect(
+      composeQuery('fire alarm')
+    ).toEqual(
+      'https://query.yahooapis.com/v1/public/yql?q=SELECT%20*%20FROM%20html%20WHERE%20url%3D%22http%3A%2F%2Fwww.ldoceonline.com%2Fdictionary%2Ffire-alarm%22'
+    )
+  });
 });
