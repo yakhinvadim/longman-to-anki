b99d6d02b2163de14cde73247f602a7f6e5ff6c9
Vadim Yakhin <yakhin.v@gmail.com>
2017-02-24 10:44:18 +0300

    beautify: App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,79 +8,77 @@ import DownloadButton from '../DownloadButton/DownloadButton';
 
 import './App.css';
 
-class App extends React.Component {
-  state = {
-    inputValue: '',
-    wordData: '',
-    showImportOptions: false
-  }
+export default class App extends React.Component {
+    state = {
+        inputValue: '',
+        wordData: '',
+        showImportOptions: false
+    }
 
-  handleInputChange = (event) => {
-    this.setState({
-      inputValue: event.target.value
-    })
-  }
+    handleInputChange = (event) => {
+        this.setState({
+            inputValue: event.target.value
+        })
+    }
 
-  handleSubmit = (event) => {
-    event.preventDefault();
+    handleSubmit = (event) => {
+        event.preventDefault();
 
-    ankifyWords(this.state.inputValue)
-      .then(result => this.setState({
-        wordData: result
-      }))
-      .catch(err => console.log(err));
-  }
+        ankifyWords(this.state.inputValue)
+            .then(result => this.setState({
+                wordData: result
+            }))
+            .catch(err => console.log(err));
+    }
 
-  handleDownload = () => {
-    this.setState({
-      showImportOptions: true
-    })
-  }
+    handleDownload = () => {
+        this.setState({
+            showImportOptions: true
+        })
+    }
 
-  render() {
-    return (
-      <div className='App'>
-        <Header/>
+    render() {
+        return (
+            <div className='App'>
+                <Header/>
 
-        <form
-          onSubmit={this.handleSubmit}
-          style={{
-            marginBottom: 20
-          }}
-        >
-          <TextField
-            hintText="example, bear, mouse"
-            floatingLabelText="Type some words, you want to learn"
-            value={this.state.inputValue}
-            onChange={this.handleInputChange}
-            fullWidth
-          />
-          <RaisedButton
-            label="Compose cards"
-            primary
-            type='submit'
-          />
-        </form>
+                <form
+                    onSubmit={this.handleSubmit}
+                    style={{
+                        marginBottom: 20
+                    }}
+                >
+                    <TextField
+                        hintText="example, bear, mouse"
+                        floatingLabelText="Type some words, you want to learn"
+                        value={this.state.inputValue}
+                        onChange={this.handleInputChange}
+                        fullWidth
+                    />
+                    <RaisedButton
+                        label="Compose cards"
+                        primary
+                        type='submit'
+                    />
+                </form>
 
-        <TextField
-          hintText="Text for anki cards will appear here"
-          multiLine
-          fullWidth
-          value={this.state.wordData}
-          rowsMax={10}
-        />
+                <TextField
+                    hintText="Text for anki cards will appear here"
+                    multiLine
+                    fullWidth
+                    value={this.state.wordData}
+                    rowsMax={10}
+                />
 
-        <DownloadButton
-          fileContent={encodeURIComponent(this.state.wordData)}
-          fileName={`longman-to-anki ${this.state.inputValue}`}
-          onClick={this.handleDownload}
-          disabled={!this.state.wordData}
-        />
+                <DownloadButton
+                    fileContent={encodeURIComponent(this.state.wordData)}
+                    fileName={`longman-to-anki ${this.state.inputValue}`}
+                    onClick={this.handleDownload}
+                    disabled={!this.state.wordData}
+                />
 
-        {this.state.showImportOptions && <ImportOptions />}
-      </div>
-    );
-  }
+                {this.state.showImportOptions && <ImportOptions />}
+            </div>
+        );
+    }
 }
-
-export default App;
