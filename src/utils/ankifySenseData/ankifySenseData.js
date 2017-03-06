014a1c1727cc23947e8c44cc81b16aea66a7ef8e
Vadim Yakhin <yakhin.v@gmail.com>
2017-03-06 22:36:12 +0300

    fix: #57 ankifySenseData now returns empty string for sense with no definition

--- src/utils/ankifySenseData/ankifySenseData.js
+++ src/utils/ankifySenseData/ankifySenseData.js
@@ -1,26 +1,19 @@
 import R from 'ramda';
 
 const ankifySenseData = ({ankifyExampleData, ankifyNoExampleData, headword, pronunciation}, senseData) => {
-  const {definition, situation, synonym, antonym} = senseData;
-  const cards = R.pipe(
-    R.ifElse(
-      R.pipe(
-        R.prop('examples'),
-        R.isEmpty
-      ),
-      R.pipe(
-        ankifyNoExampleData({headword, pronunciation}),
-        R.unless(R.is(Array), R.of)
-      ),
-      R.pipe(
-        R.prop('examples'),
-        R.map(
-          ankifyExampleData({ headword, pronunciation, definition, situation, synonym, antonym })
-        )
-      )
-    ),
+  const { definition, situation, synonym, antonym } = senseData;
+
+  const ankifySenseDataWithExamples = R.pipe(
+    R.prop('examples'),
+    R.map(ankifyExampleData({ headword, pronunciation, definition, situation, synonym, antonym })),
     R.join('\n')
-  )(senseData);
+  );
+  
+  const cards = R.cond([
+    [R.pipe(R.prop('definition'), R.isEmpty), R.always('')],
+    [R.pipe(R.prop('examples'  ), R.isEmpty), ankifyNoExampleData({headword, pronunciation})],
+    [R.T,                                     ankifySenseDataWithExamples]
+  ])(senseData);
 
   return cards;
 }
