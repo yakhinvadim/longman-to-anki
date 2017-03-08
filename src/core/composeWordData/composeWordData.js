bad45a39e9949d3bd7c86feabfb01e7cf01d96be
Vadim Yakhin <yakhin.v@gmail.com>
2017-03-08 16:33:46 +0300

    refactor: composeWordData

--- src/core/composeWordData/composeWordData.js
+++ src/core/composeWordData/composeWordData.js
@@ -1,26 +1,22 @@
 import R from 'ramda';
-
 import splitByClass from '../../utils/splitByClass/splitByClass';
 import extractHeadword from '../extractHeadword/extractHeadword';
 import extractPronunciation from '../extractPronunciation/extractPronunciation';
 import composeEntryData from '../composeEntryData/composeEntryData';
 
 const composeWordData = pageMarkup => {
-  const wordData = R.zipObj(
-    [
-      'headword',
-      'pronunciation',
-      'entries'
-    ],
-    [
-      extractHeadword(pageMarkup),
-      extractPronunciation(pageMarkup),
-      R.pipe(
-        splitByClass('ldoceEntry'),
-        R.map(composeEntryData)
-      )(pageMarkup)
-    ]
-  );
+  const headword = extractHeadword(pageMarkup);
+  const pronunciation = extractPronunciation(pageMarkup);
+  const entries = R.pipe(
+    splitByClass('ldoceEntry'),
+    R.map(composeEntryData)
+  )(pageMarkup);
+
+  const wordData = {
+    headword,
+    pronunciation,
+    entries
+  };
 
   return wordData;
  }
