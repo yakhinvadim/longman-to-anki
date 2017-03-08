b5a0f8d4724d2aaea8e90b1b1893f12112c497f2
Vadim Yakhin <yakhin.v@gmail.com>
2017-03-08 16:04:35 +0300

    refactor: extractHeadword

--- src/core/extractHeadword/extractHeadword.js
+++ src/core/extractHeadword/extractHeadword.js
@@ -1,12 +1,14 @@
 import cheerify from '../../helpers/cheerify';
 
-export default function extractHeadword(pageMarkup) {
+const extractHeadword = pageMarkup => {
   const $ = cheerify(pageMarkup);
 
   const headword =
-    $('h1.pagetitle') // I add h1 here, because page has two .pagetitle elements, and one of them is span
+    $('h1.pagetitle') // I add h1 in selector, because page has two .pagetitle elements, and one of them is span
       .text()
       .trim();
   
   return headword;
 }
+
+export default extractHeadword;
