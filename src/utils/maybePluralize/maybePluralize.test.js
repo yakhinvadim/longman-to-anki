7679ee246d984d297f2b28b970e01ac3bec4ed0d
Vadim Yakhin <yakhin.v@gmail.com>
2017-03-08 14:02:33 +0300

    feature: add maybePluralize test

---
+++ src/utils/maybePluralize/maybePluralize.test.js
@@ -0,0 +1,21 @@
+import maybePluralize from './maybePluralize';
+
+describe('maybePluralize', () => {
+  it('doesnt pluralize if count equals to 1', () => {
+    expect(
+      maybePluralize(1, 'word')
+    ).toEqual('1 word')
+  });
+    
+  it('pluralize if count is more than 1', () => {
+    expect(
+      maybePluralize(2, 'word')
+    ).toEqual('2 words')
+  });  
+
+  it('correctly pluralize if siffix is passed', () => {
+    expect(
+      maybePluralize(2, 'fox', 'es')
+    ).toEqual('2 foxes')
+  });    
+});
