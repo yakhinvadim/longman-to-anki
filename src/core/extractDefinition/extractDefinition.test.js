6460fb2195402f0dd8ac1770c4ba899870fb1481
Vadim Yakhin <yakhin.v@gmail.com>
2017-03-08 16:19:48 +0300

    refactor: extractDefinition

--- src/core/extractDefinition/extractDefinition.test.js
+++ src/core/extractDefinition/extractDefinition.test.js
@@ -2,19 +2,19 @@ import extractDefinition from './extractDefinition';
 import set from '../../mocks/set';
 
 describe('extractDefinition', () => {
-  it('gets correct definition for a normal sense', () => {
+  it('extracts correct definition for a normal sense', () => {
     expect(
       extractDefinition(set.sensesMarkup2[0])
     ).toEqual('a group of similar things that belong together or are related in some way')
   });
 
-  it('gets correct definitions for a crossref sense', () => {
+  it('extracts correct definition for a crossref sense', () => {
     expect(
       extractDefinition(set.sensesMarkup2[6])
     ).toEqual('')
   });
 
-  it('gets correct definitions for a sense with subsenses', () => {
+  it('extracts correct definition for a sense with subsenses', () => {
     expect(
       extractDefinition(set.sensesMarkup1[1])
     ).toEqual('1) if a doctor sets a broken bone, he or she moves it into position so that the bone can grow together again<br>2) if a broken bone sets, it joins together again')
