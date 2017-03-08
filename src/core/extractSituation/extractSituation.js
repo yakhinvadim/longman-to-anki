8d447efc0d850b4e459695ebee340c5dfc2dedeb
Vadim Yakhin <yakhin.v@gmail.com>
2017-03-08 16:13:46 +0300

    refactor: extractSituation

--- src/core/extractSituation/extractSituation.js
+++ src/core/extractSituation/extractSituation.js
@@ -1,7 +1,14 @@
 import cheerify from '../../helpers/cheerify';
 
-export default function extractSituation(senseMarkup) {
+const extractSituation = senseMarkup => {
   const $ = cheerify(senseMarkup);
 
-  return $('.REGISTERLAB').text().trim();
-}
+  const situation = 
+    $('.REGISTERLAB')
+      .text()
+      .trim()
+  
+  return situation;
+ }
+
+export default extractSituation;
--- src/core/extractSituation/extractSituation.test.js
+++ src/core/extractSituation/extractSituation.test.js
@@ -2,13 +2,13 @@ import extractSituation from './extractSituation';
 import set from '../../mocks/set';
 
 describe('extractSituation', () => {
-  it('gets correct situation for sense with one situation', () => {
+  it('extracts correct situation for sense with one situation', () => {
     expect(
       extractSituation(set.sensesMarkup3[0])
     ).toEqual('informal')
   });
 
-  it('gets correct situation for sense without situation', () => {
+  it('extracts correct situation for sense without situation', () => {
     expect(
       extractSituation(set.sensesMarkup2[0])
     ).toEqual('')
