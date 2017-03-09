db01589ce499d349f4cfee34383706a70753697c
Vadim Yakhin <yakhin.v@gmail.com>
2017-03-09 21:58:37 +0300

    refactor: composeSenseData

--- src/core/composeSenseData/composeSenseData.js
+++ src/core/composeSenseData/composeSenseData.js
@@ -1,22 +1,30 @@
-import composeExamplesData from '../composeExamplesData/composeExamplesData';
+import R from 'ramda';
+import splitBySelector from '../../utils/splitBySelector/splitBySelector';
+import composeExampleGroupData from '../composeExampleGroupData/composeExampleGroupData'
 import extractDefinition from '../extractDefinition/extractDefinition';
 import extractSynonym from '../extractSynonym/extractSynonym';
 import extractAntonym from '../extractAntonym/extractAntonym';
 import extractSituation from '../extractSituation/extractSituation';
+import extractExamples from '../extractExamples/extractExamples';
 
 export default function composeSenseData(senseMarkup) {
   const definition = extractDefinition(senseMarkup);
   const situation = extractSituation(senseMarkup);
   const synonym = extractSynonym(senseMarkup);
   const antonym = extractAntonym(senseMarkup);
-  const examples = composeExamplesData(senseMarkup);
+  const examples = extractExamples(senseMarkup);
+  const exampleGroups = R.pipe(
+      splitBySelector('.ColloExa, .GramExa'),
+      R.map(composeExampleGroupData)
+    )(senseMarkup);
 
   const senseData = {
     definition,
     situation,
     synonym,
     antonym,
-    examples
+    examples,
+    exampleGroups
   };
 
   return senseData;
