e33a74692b48676ba3f3159744cb2255cca1f565
Vadim Yakhin <yakhin.v@gmail.com>
2017-03-11 20:23:02 +0300

    refactor: extract markups for extractSituaton tests, add test

--- src/core/extractSituation/extractSituation.test.js
+++ src/core/extractSituation/extractSituation.test.js
@@ -1,16 +1,24 @@
 import extractSituation from './extractSituation';
-import set from '../../mocks/set';
+import markupWithSituation from '../../mocks/wordSet-entry2-sense9';
+import markupWithoutSituation from '../../mocks/wordSet-entry2-sense10';
+import markupWithTwoSituations from '../../mocks/wordDitch-entry2-sense3';
 
 describe('extractSituation', () => {
 	it('extracts correct situation for sense with one situation', () => {
 		expect(
-			extractSituation(set.sensesMarkup3[0])
-		).toEqual('informal')
+			extractSituation(markupWithSituation)
+		).toEqual('technical')
+	});
+
+	it('extracts correct situation for sense with two situations', () => {
+		expect(
+			extractSituation(markupWithTwoSituations)
+		).toEqual('spoken informal')
 	});
 
 	it('extracts correct situation for sense without situation', () => {
 		expect(
-			extractSituation(set.sensesMarkup2[0])
+			extractSituation(markupWithoutSituation)
 		).toEqual('')
 	});
 });
