84872c9f490d68585c31f14401f0adb9f624dc62
Vadim Yakhin <yakhin.v@gmail.com>
2017-03-11 19:49:33 +0300

    refactor: Totals test

--- src/components/Totals/Totals.test.js
+++ src/components/Totals/Totals.test.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import Totals from './Totals';
+import { shallow } from 'enzyme';
 
 it('renders without crashing', () => {
-	const div = document.createElement('div');
-	ReactDOM.render(<Totals />, div);
+	shallow(<Totals />);
 });
