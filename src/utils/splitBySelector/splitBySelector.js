f27f77f0976de510c169425f14c23e09445b599b
Vadim Yakhin <yakhin.v@gmail.com>
2017-03-11 11:21:50 +0300

    feature: splitBySelector now accept onlyChildren parameter

--- src/utils/splitBySelector/splitBySelector.js
+++ src/utils/splitBySelector/splitBySelector.js
@@ -1,14 +1,19 @@
 import R from 'ramda';
 import cheerify from '../../helpers/cheerify';
 
-const splitBySelector = (selector, markup) => {
+const splitBySelector = ({ selector, onlyChildren = false }, markup) => {
   const $ = cheerify(markup);
 
-  const output = $(selector)
-    .map((i, el) => $(el).html())
-    .get();
+  const markups = onlyChildren
+    ? $.root()
+        .children(selector)
+        .map((i, el) => $(el).html())
+        .get()
+    : $(selector)
+        .map((i, el) => $(el).html())
+        .get();
 
-  return output;
+  return markups;
 };
 
 export default R.curry(splitBySelector);
