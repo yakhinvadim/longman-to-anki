8afe5a22e53c1391c880fa6db1950edd34329a37
Vadim Yakhin <yakhin.v@gmail.com>
2017-03-11 19:18:38 +0300

    refactor: replace getMarkup with its content

--- src/core/wordToCards/wordToCards.js
+++ src/core/wordToCards/wordToCards.js
@@ -1,12 +1,11 @@
 import R from 'ramda';
 import composeQuery from '../../utils/composeQuery/composeQuery';
-import getMarkup from '../../helpers/getMarkup';
 import composeWordData from '../composeWordData/composeWordData';
 import ankifyWordData from '../ankifyWordData/ankifyWordData';
 
 const wordToCards = async word => {
 	const query = composeQuery(word);
-	const markup = await getMarkup(query);
+	const markup = await fetch(query).then(resp => resp.text());
 	const wordData = composeWordData(markup);
 	const cards = ankifyWordData(wordData);
 	return cards;
