6a8251f2557bf63aac44f84e887ce97207f1634f
Vadim Yakhin <yakhin.v@gmail.com>
2017-06-25 09:53:35 +0300

    refactor: replace cheerio with native DOM in extractForm, add test

--- src/core/extractForm/extractForm.js
+++ src/core/extractForm/extractForm.js
@@ -1,14 +1,17 @@
-import cheerify from '../../utils/cheerify/cheerify';
+import domify from '../../utils/domify/domify'
 
 const extractAntonym = exampleGroupMarkup => {
-	const $ = cheerify(exampleGroupMarkup);
+    const elements = [
+        ...domify(exampleGroupMarkup).querySelectorAll(
+            '.PROPFORM, .PROPFORMPREP, .COLLO, .LINKWORD, .LEXUNIT'
+        )
+    ] // all kinds of form classes
 
-	const antonym =
-		$('.PROPFORM, .PROPFORMPREP, .COLLO, .LINKWORD, .LEXUNIT') // all kinds of form classes
-			.text()
-			.trim();
+    const antonym = elements
+        .map(element => element.textContent.trim())
+        .join(' ')
 
-	return antonym;
+    return antonym
 }
 
-export default extractAntonym;
+export default extractAntonym
---
+++ src/mocks/wordSet-entry1-sense6-exampleGroup2.js
@@ -0,0 +1,4 @@
+const markup =
+    '<span class="COLLO">set something on fire/alight/ablaze</span><span class="LINKWORD"><span class="neutral span"> (</span>also</span><span class="COLLO"> set fire to something<span class="neutral span">)</span></span><span class="GLOSS"><span class="neutral span"> (=</span>make something start burning<span class="neutral span">)</span></span><span class="EXAMPLE"><span data-src-mp3="http://www.ldoceonline.com/media/english/exaProns/ldoce5_exa020223.mp3" class="speaker exafile fa fa-volume-up" title="Play Example">&nbsp;</span>Protesters set fire to two buses.</span>'
+
+export default markup
