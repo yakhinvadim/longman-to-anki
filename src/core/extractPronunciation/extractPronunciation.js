652be9b1580d9111c99bf1bdfe97cb3f6ee78fa7
Vadim Yakhin <yakhin.v@gmail.com>
2017-07-30 17:29:03 +0300

    fix: error for page with no pronunciation

--- src/core/extractPronunciation/extractPronunciation.js
+++ src/core/extractPronunciation/extractPronunciation.js
@@ -1,10 +1,14 @@
 import domify from '../../utils/domify/domify'
 
 const extractPronunciation = pageMarkup => {
+    const pronunciationWrapper = domify(pageMarkup).querySelector('.PronCodes') // there are several '.PronCodes' on each page, we need the first one
+
+    if (!pronunciationWrapper) {
+        return null
+    }
+
     const pronunciation = [
-        ...domify(pageMarkup)
-            .querySelector('.PronCodes') // there are several .PronCodes on each page, we need the first one
-            .querySelectorAll('.PRON, .AMEVARPRON')
+        ...pronunciationWrapper.querySelectorAll('.PRON, .AMEVARPRON')
     ]
         .map(node => node.textContent.trim())
         .join(' ')
--- src/core/extractPronunciation/extractPronunciation.test.js
+++ src/core/extractPronunciation/extractPronunciation.test.js
@@ -1,24 +1,25 @@
-import extractPronunciation from './extractPronunciation';
-import simpleWordMarkup from '../../mocks/wordSet';
-import irregularVerbMarkup from '../../mocks/wordGo';
-import irregularNounMarkup from '../../mocks/wordMouse';
+import extractPronunciation from './extractPronunciation'
+import simpleWordMarkup from '../../mocks/wordSet'
+import irregularVerbMarkup from '../../mocks/wordGo'
+import irregularNounMarkup from '../../mocks/wordMouse'
+import wordWithNoPronunciationMarkup from '../../mocks/wordOmg'
 
 describe('extractPronunciation', () => {
-	it('extracts correct pronunciation for ordinary word', () => {
-		expect(
-			extractPronunciation(simpleWordMarkup)
-		).toEqual('set')
-	});
+    it('extracts correct pronunciation for ordinary word', () => {
+        expect(extractPronunciation(simpleWordMarkup)).toEqual('set')
+    })
 
-	it('extracts correct pronunciation for irregular verb and american pronounciation', () => {
-		expect(
-			extractPronunciation(irregularVerbMarkup)
-		).toEqual('ɡəʊ $ ɡoʊ')
-	});
+    it('extracts correct pronunciation for irregular verb and american pronounciation', () => {
+        expect(extractPronunciation(irregularVerbMarkup)).toEqual('ɡəʊ $ ɡoʊ')
+    })
 
-	it('extracts correct pronunciation for irregular plural noun', () => {
-		expect(
-			extractPronunciation(irregularNounMarkup)
-		).toEqual('maʊs')
-	});
-});
+    it('extracts correct pronunciation for irregular plural noun', () => {
+        expect(extractPronunciation(irregularNounMarkup)).toEqual('maʊs')
+    })
+
+    it('returns null if there is no pronunciation', () => {
+        expect(extractPronunciation(wordWithNoPronunciationMarkup)).toEqual(
+            null
+        )
+    })
+})
