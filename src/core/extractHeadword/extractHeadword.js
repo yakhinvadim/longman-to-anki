d608122b20b5c257ee93707ccfd2b48d2f8cd186
Vadim Yakhin <yakhin.v@gmail.com>
2017-07-30 17:13:05 +0300

    feature: return null for non-existent words

--- src/core/extractHeadword/extractHeadword.js
+++ src/core/extractHeadword/extractHeadword.js
@@ -1,9 +1,13 @@
 import domify from '../../utils/domify/domify'
 
 const extractHeadword = pageMarkup => {
-    const headword = domify(pageMarkup)
-        .querySelector('h1.pagetitle') // I'm adding h1 in selector, because page has two .pagetitle elements, and one of them is span
-        .textContent.trim()
+    const headwordNode = domify(pageMarkup).querySelector('h1.pagetitle') // I'm adding h1 in selector, because page has two .pagetitle elements, and one of them is span
+
+    if (!headwordNode) {
+        return null
+    }
+
+    const headword = headwordNode.textContent.trim()
 
     return headword
 }
---
+++ src/utils/wordToData/wordToData.test.js
@@ -0,0 +1,24 @@
+import wordToData, { normalizeMarkup } from './wordToData'
+import fetchMock from 'fetch-mock'
+import asdfasdfMarkup from '../../mocks/wordAsdfasdf'
+import setMarkup from '../../mocks/wordSet'
+import composeQuery from '../composeQuery/composeQuery'
+
+jest.mock('../../core/composeWordData/composeWordData', () => jest.fn(a => a))
+
+fetchMock.get(composeQuery('set'), setMarkup)
+fetchMock.get(composeQuery('asdfasdf'), asdfasdfMarkup)
+
+describe('wordToData', () => {
+    it('returns null for non-existent word', () => {
+        return wordToData('asdfasdf').then(data => {
+            expect(data).toEqual(null)
+        })
+    })
+
+    it('returns correct data for real word', () => {
+        return wordToData('set').then(data => {
+            expect(data).toEqual(normalizeMarkup(setMarkup))
+        })
+    })
+})
