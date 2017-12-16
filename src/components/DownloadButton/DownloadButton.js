6b7cda4382981e2fb2a4a0f570ddeeffab8308d1
Vadim Yakhin <yakhin.v@gmail.com>
2017-12-16 17:21:26 +0300

    fix:add isRequired to some proptypes, remove impossible default values

--- src/components/DownloadButton/DownloadButton.js
+++ src/components/DownloadButton/DownloadButton.js
@@ -1,41 +1,34 @@
-import PropTypes from 'prop-types';
-import React, { PureComponent } from 'react';
-import RaisedButton from 'material-ui/RaisedButton';
+import PropTypes from 'prop-types'
+import React, { PureComponent } from 'react'
+import RaisedButton from 'material-ui/RaisedButton'
 
 export default class DownloadButton extends PureComponent {
-	static propTypes = {
-		fileContent: PropTypes.string,
-		fileName: PropTypes.string,
-		onClick: PropTypes.func,
-		disabled: PropTypes.bool
-	};
+    static propTypes = {
+        fileContent: PropTypes.string,
+        fileName: PropTypes.string,
+        onClick: PropTypes.func.isRequired,
+        disabled: PropTypes.bool
+    }
 
-	static defaultProps = {
-		fileContent: '',
-		fileName: 'longman-to-anki',
-		onClick: () => { },
-		disabled: false
-	};
+    static defaultProps = {
+        fileContent: '',
+        fileName: 'longman-to-anki',
+        disabled: false
+    }
 
-	render() {
-		const {
-			fileContent,
-			fileName,
-			onClick,
-			disabled
-		} = this.props;
+    render() {
+        const { fileContent, fileName, onClick, disabled } = this.props
 
-		return (
-			<RaisedButton
-				label="Download cards"
-				href={`data:text/plain;charset=utf-8,${fileContent}`}
-				download={fileName}
-				onClick={onClick}
-				disabled={disabled}
-				primary
-				buttonStyle={{verticalAlign: 'initial'}}
-			/>
-		);
-	}
+        return (
+            <RaisedButton
+                label="Download cards"
+                href={`data:text/plain;charset=utf-8,${fileContent}`}
+                download={fileName}
+                onClick={onClick}
+                disabled={disabled}
+                primary
+                buttonStyle={{ verticalAlign: 'initial' }}
+            />
+        )
+    }
 }
-
--- src/components/UserWords/UserWords.js
+++ src/components/UserWords/UserWords.js
@@ -1,36 +1,32 @@
-import PropTypes from 'prop-types';
-import React, { PureComponent } from 'react';
-import TextField from 'material-ui/TextField';
+import PropTypes from 'prop-types'
+import React, { PureComponent } from 'react'
+import TextField from 'material-ui/TextField'
 
 export default class UserWords extends PureComponent {
-	static propTypes = {
-		onChange: PropTypes.func,
-		value: PropTypes.string
-	};
+    static propTypes = {
+        onChange: PropTypes.func.isRequired,
+        value: PropTypes.string
+    }
 
-	static defaultProps = {
-		onChange: () => { },
-		value: ''
-	};
+    static defaultProps = {
+        value: ''
+    }
 
-	render() {
-		const {
-			onChange,
-			value
-		} = this.props;
+    render() {
+        const { onChange, value } = this.props
 
-		return (
-			<TextField
-				hintText={<span>example<br/>bear<br/>fire alarm</span>}
-				floatingLabelText="Type some words, you want to learn"
-				floatingLabelFixed={true}
-				value={value}
-				onChange={onChange}
-				fullWidth
-				multiLine
-				rows={3}
-				rowsMax={20}
-			/>
-		);
-	}
+        return (
+            <TextField
+                hintText={<span>example<br />bear<br />fire alarm</span>}
+                floatingLabelText="Type some words, you want to learn"
+                floatingLabelFixed={true}
+                value={value}
+                onChange={onChange}
+                fullWidth
+                multiLine
+                rows={3}
+                rowsMax={20}
+            />
+        )
+    }
 }
