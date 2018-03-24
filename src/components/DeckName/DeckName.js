8e34f1fa10e3eae6dbbc18fd79eba5c074248cad
Vadim Yakhin <yakhin.v@gmail.com>
2018-03-24 13:06:19 +0300

    migrate TextField to mui1

--- src/components/DeckName/DeckName.js
+++ src/components/DeckName/DeckName.js
@@ -1,27 +1,27 @@
 import PropTypes from 'prop-types'
 import React, { PureComponent } from 'react'
-import TextField from 'material-ui/TextField'
+import TextField from 'material-ui-next/TextField'
 
 export default class DeckName extends PureComponent {
-    static propTypes = {
-        onChange: PropTypes.func.isRequired,
-        value: PropTypes.string
-    }
+	static propTypes = {
+		onChange: PropTypes.func.isRequired,
+		value: PropTypes.string
+	}
 
-    static defaultProps = {
-        value: ''
-    }
+	static defaultProps = {
+		value: ''
+	}
 
-    render() {
-        const { onChange, value } = this.props
+	render() {
+		const { onChange, value } = this.props
 
-        return (
-            <TextField
-                floatingLabelText="Deck name"
-                value={value}
-                onChange={onChange}
-                fullWidth
-            />
-        )
-    }
+		return (
+			<TextField
+				label="Deck name"
+				value={value}
+				onChange={onChange}
+				fullWidth
+			/>
+		)
+	}
 }
--- src/components/UserWords/UserWords.js
+++ src/components/UserWords/UserWords.js
@@ -1,30 +1,29 @@
 import PropTypes from 'prop-types'
 import React, { PureComponent } from 'react'
-import TextField from 'material-ui/TextField'
+import TextField from 'material-ui-next/TextField'
 
 export default class UserWords extends PureComponent {
-    static propTypes = {
-        onChange: PropTypes.func.isRequired,
-        value: PropTypes.string.isRequired,
-        onKeyDown: PropTypes.func.isRequired
-    }
+	static propTypes = {
+		onChange: PropTypes.func.isRequired,
+		value: PropTypes.string.isRequired,
+		onKeyDown: PropTypes.func.isRequired
+	}
 
-    render() {
-        const { onChange, value, onKeyDown } = this.props
+	render() {
+		const { onChange, value, onKeyDown } = this.props
 
-        return (
-            <TextField
-                hintText={<span>example<br />bear<br />fire alarm</span>}
-                floatingLabelText="Type some words, you want to learn"
-                floatingLabelFixed={true}
-                value={value}
-                onChange={onChange}
-                fullWidth
-                multiLine
-                rows={3}
-                rowsMax={20}
-                onKeyDown={onKeyDown}
-            />
-        )
-    }
+		return (
+			<TextField
+				label="Type some words, you want to learn"
+				placeholder="example&#10;bear&#10;fire alarm"
+				fullWidth
+				multiline
+				rows={3}
+				rowsMax={20}
+				value={value}
+				onChange={onChange}
+				onKeyDown={onKeyDown}
+			/>
+		)
+	}
 }
