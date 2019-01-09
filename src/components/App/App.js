1122b8f732009e55cc46f7fdaea0275955bc5f20
Vadim Yakhin <yakhinvadim@gmail.com>
2019-01-09 10:28:42 -0400

    feature: add Grid

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -9,17 +9,19 @@ import maybePluralize from '../../utils/maybePluralize/maybePluralize'
 import wordToData from '../../utils/wordToData/wordToData'
 
 import GithubCorner from 'react-github-corner'
-// import LinearProgress from '@material-ui/core/LinearProgress'
+
 import Header from '../Header/Header'
 import DownloadButton from '../DownloadButton/DownloadButton'
 import ResultCards from '../ResultCards/ResultCards'
 import UserWords from '../UserWords/UserWords'
 import DeckName from '../DeckName/DeckName'
 
+import Grid from '@material-ui/core/Grid'
+
 import './App.css'
 
 const template = {
-    css: `
+	css: `
 			.card { font-family: arial; font-size: 20px; text-align: center; color: black; background-color: white; }
 			.lta-example { font-style: italic; } 
 			.lta-form { font-weight: bold; }
@@ -27,180 +29,185 @@ const template = {
 }
 
 export default class App extends React.Component {
-    state = {
-        words: [],
-        inputValue: '',
-        wordsCards: {},
-        deckName: 'English words'
-    }
+	state = {
+		words: [],
+		inputValue: '',
+		wordsCards: {},
+		deckName: 'English words'
+	}
 
-    componentDidMount() {
-        try {
-            if (localStorage.state) {
-                this.setState(JSON.parse(localStorage.state))
-            } else {
-                this.setState({ inputValue: localStorage.inputValue })
-            }
-        } catch (error) {
-            console.error(error)
-        }
-    }
+	componentDidMount() {
+		try {
+			if (localStorage.state) {
+				this.setState(JSON.parse(localStorage.state))
+			} else {
+				this.setState({ inputValue: localStorage.inputValue })
+			}
+		} catch (error) {
+			console.error(error)
+		}
+	}
 
-    componentDidUpdate(prevProps, prevState) {
-        localStorage.state = JSON.stringify(this.state)
-    }
+	componentDidUpdate(prevProps, prevState) {
+		localStorage.state = JSON.stringify(this.state)
+	}
 
-    handleDeckNameChange = event => {
-        this.setState({
-            deckName: event.target.value
-        })
-    }
+	handleDeckNameChange = event => {
+		this.setState({
+			deckName: event.target.value
+		})
+	}
 
-    handleInputChange = async event => {
-        this.setState({
-            inputValue: event.target.value
-        })
-    }
+	handleInputChange = async event => {
+		this.setState({
+			inputValue: event.target.value
+		})
+	}
 
-    handleDeleteButtonClick = wordToDelete => () => {
-        this.setState(prevState => ({
-            words: prevState.words.filter(word => word !== wordToDelete),
-            wordsCards: { ...prevState.wordsCards, [wordToDelete]: undefined }
-        }))
-    }
+	handleDeleteButtonClick = wordToDelete => () => {
+		this.setState(prevState => ({
+			words: prevState.words.filter(word => word !== wordToDelete),
+			wordsCards: { ...prevState.wordsCards, [wordToDelete]: undefined }
+		}))
+	}
 
-    handleSubmit = event => {
-        event.preventDefault()
+	handleSubmit = event => {
+		event.preventDefault()
 
-        if (this.state.inputValue === '') {
-            return
-        }
+		if (this.state.inputValue === '') {
+			return
+		}
 
-        const isMultilineInputValue = /\n/.test(this.state.inputValue)
-        const newWords = isMultilineInputValue
-            ? splitByWord(this.state.inputValue)
-            : [this.state.inputValue]
+		const isMultilineInputValue = /\n/.test(this.state.inputValue)
+		const newWords = isMultilineInputValue
+			? splitByWord(this.state.inputValue)
+			: [this.state.inputValue]
 
-        this.setState(
-            prevState => ({
-                words: [...newWords, ...prevState.words],
-                inputValue: ''
-            }),
-            () => {
-                this.state.words.map(async word => {
-                    const wordData = await wordToData(word)
+		this.setState(
+			prevState => ({
+				words: [...newWords, ...prevState.words],
+				inputValue: ''
+			}),
+			() => {
+				this.state.words.map(async word => {
+					const wordData = await wordToData(word)
 
-                    if (!wordData) {
-                        this.setState(prevState => ({
-                            wordsCards: {
-                                ...prevState.wordsCards,
-                                [word]: null
-                            }
-                        }))
-                        return
-                    }
+					if (!wordData) {
+						this.setState(prevState => ({
+							wordsCards: {
+								...prevState.wordsCards,
+								[word]: null
+							}
+						}))
+						return
+					}
 
-                    this.setState(prevState => ({
-                        wordsCards: {
-                            ...prevState.wordsCards,
-                            [wordData.headword]: normalizeWordData(wordData)
-                        },
-                        words: prevState.words.map(
-                            item => (item === word ? wordData.headword : item)
-                        )
-                    }))
-                })
-            }
-        )
-    }
+					this.setState(prevState => ({
+						wordsCards: {
+							...prevState.wordsCards,
+							[wordData.headword]: normalizeWordData(wordData)
+						},
+						words: prevState.words.map(item =>
+							item === word ? wordData.headword : item
+						)
+					}))
+				})
+			}
+		)
+	}
 
-    handleDownload = event => {
-        event.preventDefault()
+	handleDownload = event => {
+		event.preventDefault()
 
-        const cardsArr = this.state.words
-            .reverse()
-            .map(word => this.state.wordsCards[word])
-            .reduce((acc, curr) => (curr ? acc.concat(...curr) : acc), [])
-            .map(makeCard)
+		const cardsArr = this.state.words
+			.reverse()
+			.map(word => this.state.wordsCards[word])
+			.reduce((acc, curr) => (curr ? acc.concat(...curr) : acc), [])
+			.map(makeCard)
 
-        fetch('https://micro-anki-yakhinvadim.now.sh/', {
-            method: 'POST',
-            headers: new Headers({ 'Content-Type': 'application/json' }),
-            body: JSON.stringify({
-                cards: cardsArr,
-                deckName: this.state.deckName,
-                template: template
-            })
-        })
-            .then(res => res.blob())
-            .then(blob => saveAs(blob, `${this.state.deckName}.apkg`))
-            .catch(console.error)
-    }
+		fetch('https://micro-anki-yakhinvadim.now.sh/', {
+			method: 'POST',
+			headers: new Headers({ 'Content-Type': 'application/json' }),
+			body: JSON.stringify({
+				cards: cardsArr,
+				deckName: this.state.deckName,
+				template: template
+			})
+		})
+			.then(res => res.blob())
+			.then(blob => saveAs(blob, `${this.state.deckName}.apkg`))
+			.catch(console.error)
+	}
 
-    handleEnterPress = event => {
-        if (event.key === 'Enter' && event.shiftKey === false) {
-            event.preventDefault()
-            this.handleSubmit(event)
-        }
-    }
+	handleEnterPress = event => {
+		if (event.key === 'Enter' && event.shiftKey === false) {
+			event.preventDefault()
+			this.handleSubmit(event)
+		}
+	}
 
-    render() {
-        const wordsTotalNumber = this.state.words.length
-        const cardsTotalNumber = Object.values(
-            this.state.wordsCards
-        ).reduce((acc, curr) => {
-            return acc + (curr ? curr.length : 0)
-        }, 0)
+	render() {
+		const wordsTotalNumber = this.state.words.length
+		const cardsTotalNumber = Object.values(this.state.wordsCards).reduce(
+			(acc, curr) => {
+				return acc + (curr ? curr.length : 0)
+			},
+			0
+		)
 
-        const wordsTotal = maybePluralize(wordsTotalNumber, 'word')
-        const cardsTotal = maybePluralize(cardsTotalNumber, 'card')
+		const wordsTotal = maybePluralize(wordsTotalNumber, 'word')
+		const cardsTotal = maybePluralize(cardsTotalNumber, 'card')
 
-        const totals = `${wordsTotal}, ${cardsTotal}`
+		const totals = `${wordsTotal}, ${cardsTotal}`
 
-        return (
-            <div className="App">
-                <GithubCorner
-                    href="https://github.com/yakhinvadim/longman-to-anki"
-                    bannerColor="#3f51b5"
-                />
+		return (
+			<div className="App">
+				<GithubCorner
+					href="https://github.com/yakhinvadim/longman-to-anki"
+					bannerColor="#3f51b5"
+				/>
 
-                <Header />
+				<Grid container spacing={16}>
+					<Grid item item xs={12}>
+						<Header />
+					</Grid>
 
-                <form onSubmit={this.handleSubmit}>
-                    <UserWords
-                        value={this.state.inputValue}
-                        onChange={this.handleInputChange}
-                        onKeyDown={this.handleEnterPress}
-                    />
-                </form>
+					<Grid item item xs={12}>
+						<form onSubmit={this.handleSubmit}>
+							<UserWords
+								value={this.state.inputValue}
+								onChange={this.handleInputChange}
+								onKeyDown={this.handleEnterPress}
+							/>
+						</form>
+					</Grid>
 
-                {/* <LinearProgress
-					mode="determinate"
-					value={Object.keys(this.state.wordsCards).length}
-					max={this.state.words.length}
-				/> */}
+					<Grid item item xs={12}>
+						<ResultCards
+							words={this.state.words}
+							wordsCards={this.state.wordsCards}
+							onDeleteButtonClick={this.handleDeleteButtonClick}
+						/>
+					</Grid>
 
-                <ResultCards
-                    words={this.state.words}
-                    wordsCards={this.state.wordsCards}
-                    onDeleteButtonClick={this.handleDeleteButtonClick}
-                />
+					<Grid item item xs={12}>
+						<DeckName
+							value={this.state.deckName}
+							onChange={this.handleDeckNameChange}
+						/>
+					</Grid>
 
-                <DeckName
-                    value={this.state.deckName}
-                    onChange={this.handleDeckNameChange}
-                />
-
-                <div className="App__download-section">
-                    <span className="App__total">
-                        {totals}
-                    </span>
-                    <DownloadButton
-                        onClick={this.handleDownload}
-                        disabled={!cardsTotalNumber}
-                    />
-                </div>
-            </div>
-        )
-    }
+					<Grid item item xs={12}>
+						<div className="App__download-section">
+							<span className="App__total">{totals}</span>
+							<DownloadButton
+								onClick={this.handleDownload}
+								disabled={!cardsTotalNumber}
+							/>
+						</div>
+					</Grid>
+				</Grid>
+			</div>
+		)
+	}
 }
