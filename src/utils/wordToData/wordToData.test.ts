93edd2ec7f3258dddc4a6b1321378513e3ef61ba
Vadim Yakhin <yakhinvadim@gmail.com>
2019-02-04 14:25:02 -0400

    feature: add offline test for wordToData

--- src/utils/wordToData/wordToData.test.ts
+++ src/utils/wordToData/wordToData.test.ts
@@ -9,8 +9,15 @@ jest.mock('../../core/composeWordData/composeWordData', () => jest.fn(a => a))
 
 fetchMock.get(composeQuery('set'), setMarkup)
 fetchMock.get(composeQuery('asdfasdf'), asdfasdfMarkup)
+fetchMock.get(composeQuery('offline-request'), { throws: 'Failed to fetch' })
 
 describe('wordToData', () => {
+    it('returns correct status for offline request', () => {
+        return wordToData('offline-request').then(data => {
+            expect(data).toEqual({ status: WordFetchError.Offline })
+        })
+    })
+
     it('returns correct status for non-existent word', () => {
         return wordToData('asdfasdf').then(data => {
             expect(data).toEqual({ status: WordFetchError.NotFound })
