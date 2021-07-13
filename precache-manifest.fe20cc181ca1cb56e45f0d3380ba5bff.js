f3cc5ea02a0c409f2476bd128128910fc7a06096
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-07-13 16:36:22 +0000

    Deploy yakhinvadim/longman-to-anki to github.com/yakhinvadim/longman-to-anki.git:gh-pages

---
+++ precache-manifest.fe20cc181ca1cb56e45f0d3380ba5bff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "014d77bef1a92fdf8b5627352f4018fb",
+    "url": "/longman-to-anki/index.html"
+  },
+  {
+    "revision": "311a6ec79ae91a2f6598",
+    "url": "/longman-to-anki/static/css/main.7fc851ce.chunk.css"
+  },
+  {
+    "revision": "f3e0eb25c4e34b2de302",
+    "url": "/longman-to-anki/static/js/2.2477eb53.chunk.js"
+  },
+  {
+    "revision": "311a6ec79ae91a2f6598",
+    "url": "/longman-to-anki/static/js/main.353b4ad4.chunk.js"
+  },
+  {
+    "revision": "7d139d21f7533b3f1e46",
+    "url": "/longman-to-anki/static/js/runtime-main.af7d0b64.js"
+  }
+]);
