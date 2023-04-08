164d0067b134b01854bf893352c89e315e2b98a0
Vadim Yakhin <yakhin.v@gmail.com>
2023-04-08 14:28:55 -0700

    New alert

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,3 +1,4 @@
+import { Alert, AlertTitle } from '@material-ui/lab'
 import React, { PureComponent } from 'react'
 import GithubCorner from 'react-github-corner'
 
@@ -9,6 +10,28 @@ export default class Header extends PureComponent {
                     href="https://github.com/yakhinvadim/longman-to-anki"
                     bannerColor="#3f51b5"
                 />
+                <Alert severity="info">
+                    <AlertTitle>
+                        👋 Hi! I'm Vadim, the creator of Longman to Anki. I have
+                        a favor to ask.
+                    </AlertTitle>
+                    <br />I recently started a new project called{' '}
+                    <strong>The News Minimalist</strong>, aimed at helping
+                    people stay informed without feeling overwhelmed. Every day,
+                    I use ChatGPT to analyze the top 1000 news stories and
+                    select the most important ones. Then, I send a summary email
+                    to my subscribers.
+                    <br />
+                    <br />
+                    If you're interested, please{' '}
+                    <a href="https://www.newsminimalist.com/subscribe">
+                        support me by subscribing
+                    </a>
+                    ! And if you like it, please share it with your friends!
+                    <br />
+                    <br />
+                    Thank you!
+                </Alert>
                 <header>
                     <h1>Longman to Anki</h1>
                     <p>A web app to help you learn English with Anki</p>
